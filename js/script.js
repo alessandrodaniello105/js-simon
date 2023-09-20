55cccd6b5d4bfbf620cd4ac99fe37565d0dbed44
alessandrodaniello105 <kanymsevement@gmail.com>
2023-09-20 13:37:09 +0200

    [add]: unique random number verify

--- js/script.js
+++ js/script.js
@@ -1,10 +1,29 @@
 // 1. Creo un array che conterrà i numeri generati
 const numRandom = [];
 
-
-
-// 2. Creo una funzione di generazione randomica di 5 numeri
+// 2. Creo una funzione di generazione randomica di 5 numeri...
 const randomGen = () => Math.ceil(Math.random() * 10);
 
+const numTarget = 5;
+let counterNum = 0;
 
-console.log(randomGen());
+while (counterNum < numTarget ) {
+  
+  num = randomGen();
+  
+  counterNum++;
+  // 2. ...con verifica
+  if (numRandom.includes(num)) {
+    counterNum--;
+
+    const numNoNo = [];
+    numNoNo.push(num)
+    num = randomGen();
+  } else {
+
+    numRandom.push(num);
+  }
+
+
+};
+console.log(numRandom);
