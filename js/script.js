f60e1e2a718f1774528bd5be83229c0177114885
alessandrodaniello105 <kanymsevement@gmail.com>
2023-09-20 13:28:20 +0200

    [add]: random number generator

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+// 1. Creo un array che conterrà i numeri generati
+const numRandom = [];
+
+
+
+// 2. Creo una funzione di generazione randomica di 5 numeri
+const randomGen = () => Math.ceil(Math.random() * 10);
+
+
+console.log(randomGen());
